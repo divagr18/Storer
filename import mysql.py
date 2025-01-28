eb2a758518a76dd0ada2eac47a32e80c230a687e
Divyansh Agrawal <keshav.agr2007@gmail.com>
2025-01-28 13:16:30 +0530

    nice

--- import mysql.py
+++ import mysql.py
@@ -3,9 +3,23 @@ import mysql.connector
 conn = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="1234",
+    password="1234" ,
+    database = 'storer',
     port = 3308
 )
 
-print("Connected to MySQL!", conn.is_connected())
+cursor = conn.cursor()
+
+# Test connection by fetching some data from Products table
+cursor.execute("""
+    INSERT INTO Products (name, description, price, stock_level, category)
+    VALUES (%s, %s, %s, %s, %s)
+""", ("Product A", "A description for Product A", 19.99, 100, "Category 1"))
+conn.commit()
+cursor.execute("SELECT * FROM Products;")
+products = cursor.fetchall()
+for product in products:
+    print(product)
+
+cursor.close()
 conn.close()
